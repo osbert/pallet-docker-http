ab621a701ef4b34fe9da40d9b850b87f740a1c81
Osbert Feng <osbert@iterinc.com>
2014-04-16 14:36:59 -0700

    Add pallet to dependencies.

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://github.com/osbert/pallet-docker-http"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.cloudhoist/pallet "0.7.2"]]
+  :local-repo-classpath true
+  :repositories {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"
+                 "sonatype" "https://oss.sonatype.org/content/repositories/releases/"})
